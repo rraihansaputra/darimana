3e5a170971e46ac2220a15a04a423550bf11cd31
rs <rraihansaputra@gmail.com>
2019-03-04 08:34:34 +0700

    WIP: algo for the distance return

---
+++ map-sort-test.js
@@ -0,0 +1,35 @@
+// Tested this on the browser. Algorithm is still not fully coded
+
+// PSEODOCODE ALGO
+// get the latlong of the place selected and filter current station by straightline distance (15km)
+
+// async get the distance for each station and put them into
+// {id: stationid, dist: returned distance} objects and array them
+//      need to have an individual getGMapsDistance() that calls axios and returns the object
+
+// Assume this is the result of the distance from googlemaps
+var wota = [
+    { id: 'this is not', dist: 3.4 },
+    { id: 'other station', dist: 0.5 },
+    { id: 'yet another station', dist: 4.4 },
+    { id: 'straight to hell', dist: 99 },
+    { id: 'home', dist: 0.1 }
+]
+
+console.log(
+    // returns the sorted array based on the dist property of the object
+    wota.sort(function(a,b) {
+        if (a.dist > b.dist) {
+            return 1
+        } 
+        if (a.dist < b.dist) {
+            return -1
+        } return 0
+    });
+
+)
+
+// get the objects added with info from the json
+//      add stop information (stop name)
+//      add lines information
+//      add tracks information
