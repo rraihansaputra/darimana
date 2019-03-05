29c424ae4eee022b8963ce6d9d78eb1c2264f110
rs <rraihansaputra@gmail.com>
2019-03-05 09:19:21 +0700

    further integration with vue; progressing in creating the app

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 var appInput = new Vue ({
     el: '.app-input',
-    mounted: function() {
-      this.autocomplete = new google.maps.places.Autocomplete(
-      this.$refs.autocomplete)
-      this.autocomplete.setComponentRestrictions(
+    data: {
+        place: {},
+        stopsBase:{},
+        stopsInfo: {},
+        tracksInfo: {},
+        linesInfo: {},
+    },
+    methods: {
+      gAutocomplete: function() {
+        this.autocomplete = new google.maps.places.Autocomplete(
+          this.$refs.autocomplete)
+          this.autocomplete.setComponentRestrictions(
               {'country': ['id']}
           );
-
-      // Set the data fields to return when the user selects a place.
-      // Set fields to [''] to get all the data
-      this.autocomplete.setFields([
-                  'address_components', 
-                  'name',
-                  'id',
-                  'place_id',
-                  'reference',
-                  'url',
-                  'geometry',
-                  ]);
-       
-      this.autocomplete.addListener('place_changed', () => {
-              var place = this.autocomplete.getPlace()
-              console.log(place)
-              if (!place.geometry) {
+    
+          // Set the data fields to return when the user selects a place.
+          // Set fields to [''] to get all the data
+          this.autocomplete.setFields([
+            'address_components', 
+            'name',
+            'id',
+            'place_id',
+            'reference',
+            'url',
+            'geometry',
+            ]);
+           
+          this.autocomplete.addListener('place_changed', () => {
+            var place = this.autocomplete.getPlace()
+            this.place = place
+            if (!place.geometry) {
               // User entered the name of a Place that was not suggested and
               // pressed the Enter key, or the Place Details request failed.
               window.alert("No details available for input: '" + place.name + "'");
               return;
-              }
-      });
+            }
+          });
+      },
+      getData: function(){
+        axios
+        .get('krl_stops.json')
+        .then(response => {
+          this.stopsBase = response.data;
+        });
+        axios
+          .get('all.json')
+          .then(response => {
+            this.stopsInfo = response.data.stops;
+            this.tracksInfo = response.data.tracks;
+            this.linesInfo = response.data.lines;
+          });
+      },
+    },
+    mounted: function() {
+      this.gAutocomplete()
+      this.getData()
     }
-  })
+  });
--- map-sort-test.js
+++ map-sort-test.js
@@ -2,6 +2,12 @@
 
 // PSEODOCODE ALGO
 // get the latlong of the place selected and filter current station by straightline distance (15km)
+var stops = []; // list of stops
+var place = new google.maps.LatLng(lat,lng); //get coords
+
+var newstops = stops.map() //map the thing have new value based on computeDistanceBetween
+//filter distance <= 10k
+const result = newstops.filter(stop => stop.ddist < 10000)
 
 // async get the distance for each station and put them into
 // {id: stationid, dist: returned distance} objects and array them
@@ -25,11 +31,18 @@ console.log(
         if (a.dist < b.dist) {
             return -1
         } return 0
-    });
-
-)
+    }));
 
 // get the objects added with info from the json
 //      add stop information (stop name)
 //      add lines information
 //      add tracks information
+
+google.maps.geometry.spherical.computeDistanceBetween(
+    new google.maps.LatLng(6.2326307,106.8237375),
+    new google.maps.LatLng(-6.22, 106.85))
+
+    google.maps.geometry.spherical.computeDistanceBetween(
+        {lat:-6.2326307,lng:106.8237375},
+        {lat:-6.22, lng :106.85}
+        )
